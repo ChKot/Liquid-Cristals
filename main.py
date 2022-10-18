97515535375110b9750212521d0be6b4df3896c0
ChKot <romakirillov2000@gmail.com>
2022-10-18 23:58:08 +0300

    median filter upd.
    Median filter was fixed

--- main.py
+++ main.py
@@ -19,31 +19,32 @@ def mid_filter(data, N):
             y.append(data[i, 1])
     return y
 
-# def med_filter(data, N):
-#     temp_arr = []
-#     temp_arr2 = []
-#     y = []
-#     for i in range(0, N):
-#         temp_arr.append(0)
-#     for i in range(np.shape(data)[0]):
-#         for j in range(0, N-1):
-#             temp_arr[j] = temp_arr[j+1]
-#         temp_arr[N-1] = data[i, 1]
-#         for j in range(0, N):
-#             temp_arr2[j] = temp_arr[j]
-#         temp_arr2.sort()
-#         if i >= N:
-#             if N % 2:
-#                 y.append((temp_arr2[N/2]+temp_arr2[(N/2)-1])/2)
-#             else:
-#                 y.append(temp_arr2[N])
-#         else:
-#             y.append(data[i, 1])
-#     return y
+def med_filter(data, N):
+    temp_arr = []
+    temp_arr2 = []
+    y = []
+    for i in range(0, N):
+        temp_arr.append(0)
+        temp_arr2.append(0)
+    for i in range(np.shape(data)[0]):
+        for j in range(0, N-1):
+            temp_arr[j] = temp_arr[j+1]
+        temp_arr[N-1] = data[i, 1]
+        for j in range(0, N-1):
+            temp_arr2[j] = temp_arr[j]
+        temp_arr2.sort()
+        if i >= N:
+            if N % 2:
+                y.append(temp_arr2[N / 2])
+            else:
+                y.append((temp_arr2[int(N / 2)] + temp_arr2[int((N / 2) - 1)]) / 2)
+        else:
+            y.append(data[i, 1])
+    return y
 
 arduino = serial.Serial('COM3', 1000000, timeout=1)
 time.sleep(2)
-arduino.write('s10000000'.encode())
+arduino.write('s1000000'.encode())
 time.sleep(0.5)
 while not arduino.in_waiting:
     time.sleep(0.01)
@@ -66,11 +67,9 @@ for i in buff:
 
 filtred_data1 = mid_filter(data, 12)
 data= np.column_stack([data, filtred_data1])
-# filtred_data2 = med_filter(data, 6)
-# data= np.column_stack([data, filtred_data2])
-# print(data)
-# print(data[1])
-# print(np.shape(data))
+filtred_data2 = med_filter(data, 6)
+data= np.column_stack([data, filtred_data2])
+
 
 t_name = time.localtime()
 name = time.strftime("%H_%M_%S", t_name)
